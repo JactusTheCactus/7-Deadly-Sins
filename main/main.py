de2cde0eac9e5e883cea59b1bce94da48c33dc04
JactusTheCactus <threateninglyamusing@gmail.com>
2025-01-07 14:50:38 -0800

    Update main.py

--- main/main.py
+++ main/main.py
@@ -12,9 +12,7 @@ weapon = data["weapon"]
 colour = data["colour"]
 power = data["power"]
 race = data["race"]
-# Function to convert JSON to HTML
 def json_to_html(json_file, html_file):
-    # Open and load the JSON data
     with open(json_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
     fullName = f"{name}, The {animal} Sin of {sin}"
@@ -58,13 +56,11 @@ def json_to_html(json_file, html_file):
     """
     html_content = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', html_content)
     html_content = re.sub(r'\*(.*?)\*', r'<i>\1</i>', html_content)
-    # Write the HTML content to a file
     with open(html_file, 'w', encoding='utf-8') as f:
         f.write(html_content)
 sins = Path("main/sins/json")
 sinJSON = [item.name for item in sins.iterdir()]
 sinHTML =  [item[:-4] + "html" for item in sinJSON]
-# Use Python string formatting to insert variables into the HTML template
 home_html = f"""
 <!DOCTYPE html>
 <html lang="en">
@@ -99,29 +95,22 @@ def createfile(directory,name,type,content):
 	with open(f"{directory}/{name}", "w") as file:
 		file.write(content)
 def json_to_md_table(directory):
-    # List all .json files in the directory
     json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
-    # Initialize a list to store rows for the markdown table
     table_rows = []
-    # Process each json file
     for json_file in json_files:
         with open(f"{directory}/{json_file}", "r") as file:
             data = json.load(file)
             row = f"|{name}|{sin}|{animal}|{weapon}|{colour}|{power}|{race}|"
             table_rows.append(row)
-    # Create the markdown table header
     table_header = "|Name|Sin|Mark|Weapon|Colour|Power|Race|\n"
     table_header += "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n"
     table_footer = "\n[Home](home/home.html)"
-    # Combine header and rows to form the full markdown content
     md_content = f"There are __Seven Deadly Sins__. Here is a table:\n\n{table_header}{'\n'.join(table_rows)}\n\n{table_footer}"
-    # Use the createfile function to save the markdown content
     createfile("./", "README", "md", md_content)
 json_to_md_table('main/sins/json')
 for i in range(len(sinJSON)):
     json_file = f"main/sins/json/{sinJSON[i]}"
     html_file = f"main/sins/html/{sinHTML[i]}"
     json_to_html(json_file, html_file)
-# Write the home page separately after generating the sin pages
 with open("main/home/home.html", "w", encoding="utf-8") as file:
     file.write(home_html)
