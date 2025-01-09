6291ad50931f828ac346c2a3c9fcee1aebf88cd9
Devin <threateninglyamusing@gmail.com>
2025-01-09 15:57:23 -0800

    -

--- tallpage.py
+++ tallpage.py
@@ -204,7 +204,7 @@ html_content = f"""
 html_content += f"""
             <section id="aspect" class="wrapper style1 fullscreen fade-up">
                 <div class="inner">
-                    <h1 style="font-size: 8rem;">
+                    <h1 style="font-size: 5rem;">
                         The Capital Aspects
                     </h1>
                 </div>
