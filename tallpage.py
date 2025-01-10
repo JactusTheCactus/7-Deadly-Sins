e424d8da4b5d1b73d55baf7df218ae5e118321e5
Devin <threateninglyamusing@gmail.com>
2025-01-09 18:49:02 -0800

    -

--- tallpage.py
+++ tallpage.py
@@ -205,7 +205,7 @@ def genAspect(aspectAlignment):
                         Superpower: {escape(power)}<br>
                         Gear-Colour: {escape(colour)}<br>
                         Weapon: {escape(weapon)}<br>
-                        Inverse {escape(inverseAspect.capitalize())}: <a href="#{escape(inverse)}">{escape(title(inverse))}</a>
+                        Inverse {escape(inverseAspect.capitalize())}: <a href="#{escape(inverse)}">{escape(full(inverse))}</a>
                     </p>
                 </div>
             </section>
